f657a591226a2be57d7e4ebbc833178c7ad238f8
Yuri Vaillant <yurivaillant@gmail.com>
2018-03-22 23:51:58 +0100

    added basic markup to app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
+import companyLogo from './sv-logo.svg';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div class="App">
+		 <div id="sidebar" >
+			<img src={companyLogo} className="Company-logo" alt="Sharevalue logo"
+			<ul className="App-menu">
+				<li><i class=""><span>Dashboard</span></li>
+				<li><i class=""><span>Mijn CV</span></li>
+				<li><i class=""><span>Team CV's</span></li>				
+			</ul>
+		</div>
+		<div id="main">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<i class="fa fa-magnifier" ></i>
+				<input type="text" placeholder="Zoek CV's"/>				
+			</header>			
+			<p className="App-intro">
+			  <div className="Search-results" >
+				<ul>{}</ul>
+			  </div>
+			  <Switch>
+				  <Route exact path='/' component={Dashboard}/>
+				  <Route path='/detail/:resumeId' component={ResumeDetail}/>
+				  <Route path='/overview/' component={ResumeOverview}/>
+				</Switch>
+			</p>
+		</div>
+		<div id="modal" >
+		</div>
       </div>
     );
   }
